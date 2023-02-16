ff0b0c1e41f37c76b4bc989363f653e7ffa28065
msteele919 <msteele919@gmail.com>
2023-02-16 14:04:50 +0100

    här kommer journaler_main appen klar

--- python/assignment/journaler_main.py
+++ python/assignment/journaler_main.py
@@ -33,12 +33,123 @@ def add_journal():
     specialist = input("Specialistensid: ")
     prognos = input("Prognos enligt regionenskatalog (siffrakod): ")
     anteckningar = input("Specialistens anteckningar: ")
-    medicin = input("Namnet på medicin given? Om inga medicin given skriv null:  ")
+    medicin = input("Elevs medicin? Om inga medicin given skriv 'null':  ")
     nytidskapad = input("Har en ny tid bokats? Skriv True = ny tid bokad eller False = ingen ny tid bokad : ")
     ny_journal = Journaler(datum=datum, student_personnummer= student_personnummer , specialist= specialist , 
                            prognos= prognos , anteckningar= anteckningar , medicin= medicin , nytidskapad= nytidskapad)
+    res = requests.post(url("/add_journal"), json = ny_journal.dict())
+    print(res)
+
+def hämta_journal(): 
+    jounraler = []
+    print("Hämta alla journaler")
+    res = requests.get(url("/journaler"))
+    if not res.status_code == 200: 
+        return 
+    data = res.json()
+    for journal in data: 
+        journal = Journaler(**journal)
+        print("________")
+        print(f"Journal id: {journal.journal_id}")
+        print(f"Datum : {journal.datum}")
+        print(f"Elevs personnummer : {journal.student_personnummer}")
+        print(f"Specialists ID : {journal.specialist}")
+        print(f"Elevs prognos : {journal.prognos}")
+        print(f"Anteckningar : {journal.anteckningar}")
+        print(f"Elevs medicin : {journal.medicin}")
+        print(f"Nytidskapad : {journal.nytidskapad}")
+
+def uppdatera_journal(journalerna: List[Journaler]): 
+    print("Uppdatera en elevs journal", journalerna)
+    journal_att_uppdatera = input("Journal ID av journalen som ska uppdateras: ")
+    if not str.isdigit(journal_att_uppdatera):
+        print("journal ID är en integer")
+        return 
+    
+    index= None 
+    for i, journal in enumerate(journalerna): 
+        print(journal.journal_id)
+        if journal.journal_id == int(journal_att_uppdatera): 
+            index = i
+            break 
+
+    if index == None: 
+        print("Ingen journal")
+        return
+    journal = journalerna[index]
+
+    datum = input("Journal datum(lämna om det är samma): ")
+    student_personnummer = ("Elevs personnummer (lämna om det är samma): ")
+    specialist = input("Specialistensid (lämna om det är samma): ")
+    prognos = input("Prognos enligt regionenskatalog (siffrakod) (lämna om det är samma): ")
+    anteckningar = input("Specialistens anteckningar (lämna om det är samma): ")
+    medicin = input("Elevs medicin? Om inga medicin given skriv 'null' (lämna om det är samma):  ")
+    nytidskapad = input("Har en ny tid bokats? Skriv True = ny tid bokad eller False = ingen ny tid bokad  (lämna om det är samma): ")
+
+    if not datum:
+        datum = journal.datum
+    if not student_personnummer:
+        student_personnummer = journal.student_personnummer
+    if not specialist:
+        specialist= journal.specialist
+    if not prognos:
+        prognos = journal.prognos
+    if not anteckningar:
+        anteckningar = journal.anteckningar
+    if not medicin:
+        medicin = journal.medicin
+    if not nytidskapad:
+        nytidskapad = journal.nytidskapad
+
+    ny_journal = Journaler(datum=datum, student_personnummer= student_personnummer , specialist= specialist , 
+                           prognos= prognos , anteckningar= anteckningar , medicin= medicin , nytidskapad= nytidskapad)
+    res = requests.put(url(f"/uppdatera_journal/{journal_att_uppdatera}"), json = ny_journal.dict())
+    print(res.json())
+def radera_journal():
+    ("Du har valt radera journal. Ange lösenord för att kunna göra detta:")
+    lösnord= input()
+    if lösnord != 1234:
+        print("Lösenord är fel")
+        return
+    journal_att_radera = input("Journal Id av journal du vill radera: ")
+    if not str.isdigit(journal_att_radera):
+        print("Ids är integer")
+        return 
+    res = requests.delete(url(f"/delete_journal/{journal_att_radera}"))
+    print(res.json())    
     
     
+    
+    
+    pass 
+
+
+def main(): 
+    print_menu()
+    val = input("Vad vill du göra?: ")
+    val = val.strip()
+    if not str.isdigit(val):
+        print("Välj ett val som finns")
+        return 
+    
+    match int(val):
+        case 1: 
+            add_journal()
+        case 2: 
+            journalerna = hämta_journal()
+        case 3: 
+            journalerna = hämta_journal() 
+            uppdatera_journal(journalerna)
+        case 4: 
+            radera_journal()
+        case 5:
+            exit()
+        case _:
+            print("Välj ett av val i listan") 
+
+while __name__ == "__main__":
+    main()
+
 
 
 
